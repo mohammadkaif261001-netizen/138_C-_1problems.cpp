e0cd2f589bc7487b7432d725d15e996f2f54124b
Kaif <mohammadkaif261001@gmail.com>
2026-03-06 22:22:23 +0600

    problem_08.cpp

---
+++ problem_08.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if(num>0){
+        cout<<num<<" is positive number.";
+    }
+    else if(num<0){
+        cout<<num<<" is negetive number.";
+    }
+    else{
+        cout<<num<<" is zero.";
+    }
+    return 0;
+
+}
