45f1f5f88f2e82660729dc805d2a7686745519f0
Kaif <mohammadkaif261001@gmail.com>
2026-03-08 15:35:31 +0600

    problem_14.cpp

---
+++ problem_14.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a charecter: ";
+    cin>>ch;
+    cout<<"ASCII value of "<<ch<<" : "<<int(ch)<<endl;
+    return 0;
+}
