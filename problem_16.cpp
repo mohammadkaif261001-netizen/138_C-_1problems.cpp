9e5e6a1d5c2af899dcbedaa816f1547c30101eb6
Kaif <mohammadkaif261001@gmail.com>
2026-03-11 23:01:28 +0600

    problem_16.cpp

---
+++ problem_16.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    long long fact=1;
+    cout<<"Enter a number: ";
+    cin>>num;
+    for(int i=1; i<=num; i++){
+        fact=fact*i;
+    }
+    cout<<"Factorial of "<<num<<" = "<<fact<<endl;
+    return 0;
+}
